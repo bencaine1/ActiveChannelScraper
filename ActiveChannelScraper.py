42b6a7a8b1072edcad47fec74d302a825d14b477
bencaine1 <bencaine1@gmail.com>
2014-06-25 15:57:24 -0400

    status

--- ActiveChannelScraper.py
+++ ActiveChannelScraper.py
@@ -12,14 +12,16 @@ import sys
 #import pywin32
 
 class ActiveChannelDataPt:
-    def __init__(self, system, channel, filename):
+    def __init__(self, system, channel, filename, status):
         self.system = system
         self.channel = channel
         self.filename = filename
+        self.status = status
     def __str__(self):
         s = 'System: ' + self.system + '\n'
-        s += 'Channel: ' + self.channel + '\n'
+        s += 'Channel: ' + str(self.channel) + '\n'
         s += 'Filename: ' + self.filename + '\n'
+        s += 'Status: ' + self.status + '\n'
         return s
 
 # connect to db
@@ -46,6 +48,8 @@ eq_cnxn.autocommit = True
 eq_cursor = eq_cnxn.cursor()
 
 paths = {}
+status_paths = {}
+
 res = eq_cursor.execute("""
 select tester_nm from MaccorList
 where active_val = 'y'
@@ -54,32 +58,31 @@ where active_val = 'y'
 for row in res:
     path = '\\\\' + row[0].rstrip(' ') + '.24m.local\\Maccor\\System\\' + row[0].rstrip(' ') + '\\Active'
     paths[path] = row[0].rstrip(' ')
-
-
-#paths = {
-#r'\\maccor-a.24m.local\Maccor\System\MACCOR-A\Active':'Maccor-A',
-#r'\\maccor-b.24m.local\Maccor\System\MACCOR-B\Active':'Maccor-B',
-#r'\\maccor-m.24m.local\Maccor\System\MACCOR-M\Active':'Maccor-M',
-#r'\\maccor-n.24m.local\Maccor\System\MACCOR-N\Active':'Maccor-N',
-#r'\\maccor-o.24m.local\Maccor\System\MACCOR-O\Active':'Maccor-O'
-#}
+    status_paths[row[0].rstrip(' ')] = '\\\\' + row[0].rstrip(' ') + '.24m.local\\Maccor\\RemoteCtrl'
 
 data = []
 for p in paths.keys():
     for f in os.listdir(p):
         if os.path.isfile(os.path.join(p,f)):
-            system, channel, filename = ('',)*3
+            system, channel, filename, status = ('',)*4
             filename, sep, channel = f.rpartition('.')
             try:
                 channel = int(channel)
             except:
                 print 'non-integer extension for file ', f
-#            m = re.search('\.(?P<number>[0-9]{3})', f)
-#            if m:
-#                channel = m.group('number')
             system = paths[p] # e.g. 'Maccor-A'
-            a = ActiveChannelDataPt(system, channel, filename)
+            for sf in os.listdir(status_paths[paths[p]]):
+                s_filename, s_sep, s_channel = sf.rpartition('.')
+                if s_channel == str(channel).zfill(4):
+                    if 'complete' in s_filename.lower():
+                        status = 'complete'
+                    elif 'running' in s_filename.lower():
+                        status = 'running'
+            a = ActiveChannelDataPt(system, channel, filename, status)
             data.append(a)
+
+#for p in status_paths:
+    
 
 #for a in data:
 #    print a
@@ -94,9 +97,9 @@ sys.stdout.write('Populating table')
 for a in data:
     sys.stdout.write('.')
     cursor.execute("""
-    insert into ActiveChannelLog (System, Channel, Filename)
-    values (?,?,?);
-    """, a.system, a.channel, a.filename)
+    insert into ActiveChannelLog (System, Channel, Filename, Status)
+    values (?,?,?,?);
+    """, a.system, a.channel, a.filename, a.status)
 print
 
 #close up shop
